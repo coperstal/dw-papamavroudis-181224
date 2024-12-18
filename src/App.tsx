ba517df6217e7c5796657b38aa1446936bd9d902
MILTIADIS PAPAMAVROUDIS <papamavroudis@yahoo.gr>
2024-12-18 12:18:00 +0200

    Miltiadis

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// POSTS Endpoint
+// https://jsonplaceholder.typicode.com/posts
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Post {
+  id: string;
+  title: string;
+  body: string;
 }
 
-export default App
+interface PostCardProps {
+  post: Post;
+}
+
+function PostCard({ post }: PostCardProps) {
+  return (
+    <div>
+      <img src="https://picsum.photos/200/300"></img>
+      {/* TODO: Add an image here that will fit in 200x200 without distortion -> "https://picsum.photos/200/300" */}
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      {/* TODO: Add Post Title */}
+      {/* TODO: Add Post body with 200 chars max and add ... at the end */}
+    </div>
+  );
+}
+
+export default function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        { method: "GET" }
+      );
+      if (!response.ok) {
+        throw new Error(`Response Status ${response.status}`);
+      } else {
+        const json: Post[] = await response.json();
+        setPosts(json);
+      }
+    };
+
+    getData();
+
+  }, []);
+
+  
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  // TODO: Fetch Posts
+  // TODO: Filter posts by post title based on the given query
+   const filterQuery=()=>{
+    const searchedPost= posts.filter((post)=>post.title==searchText)
+    setPosts(searchedPost)
+   }
+
+  return (
+    <div className="App">
+      {/*  TODO: Implement search */}
+      <input placeholder="Search" onChange={onChangeHandler} />
+
+      
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {/* TODO: Render the PostCard for each post */}
+
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post}></PostCard>
+        ))}
+      </div>
+    </div>
+  );
+}
